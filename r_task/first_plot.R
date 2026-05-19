dccfc65e53ddd132ad91f6463b82682d50f8fc94
Abbiasov Maksim <abbiasov.fpw@yandex.ru>
2026-05-20 02:12:58 +0300

    Add R script and first plot

---
+++ r_task/first_plot.R
@@ -0,0 +1,12 @@
+genes <- c("BRCA1", "TP53", "EGFR")
+expression <- c(12.5, 45.2, 30.1)
+condition <- c("Control", "Treatment", "Treatment")
+
+exp_data <- data.frame(genes, expression, condition)
+
+str(exp_data)
+
+png("r_task/expression_plot.png")
+barplot(exp_data$expression, names.arg = exp_data$genes, xlab = "Gene", ylab = "Expression")
+dev.off()
+
